2b7950686841ba0a4c361fefe9725e1e61f87b4e
colas--fiaschi <jules.colas-fiaschi@etud.univ-eiffel.fr>
2025-05-21 17:50:35 +0200

    page acceuil

---
+++ Html/acceuil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>acceuil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="col col-m-12" style="background-color: rgb (15 ,39 ,59);">
+        <header> 
+            <h1>Acceuil</h1>
+            <h1>Reservation</h1>
+            <h1>Aide</h1>
+        </header>
+    </div>
+    
+</body>
+</html>
